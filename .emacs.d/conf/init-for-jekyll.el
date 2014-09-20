262f624e1498ced838c29241a75a48bbf7685563
Naoki Ueda <su104003@gmail.com>
2014-09-20 11:18:13 +0900

    modified publish for jekyll

--- .emacs.d/conf/init-for-jekyll.el
+++ .emacs.d/conf/init-for-jekyll.el
@@ -1,33 +1,20 @@
 (setq org-publish-project-alist
       '(
-        ("org"
+        ("jekyll"
          ;; Path to your org files
-         :base-directory "~/workspace/dotfiles/.emacs.d/__org"
+         :base-directory "~/workspace/tpc-jekyll/_org"
          :base-extension "org"
 
          ;; Path to your jekyll project
-         :publishing-directory "~/workspace/dotfiles/.emacs.d/__org/my-jekyll/_posts"
+         :publishing-directory "~/workspace/tpc-jekyll/_posts/org"
          :recursive t
          :publishing-function org-html-publish-to-html
          :headline-levels 4
-         :html-extension "markdown"
-         :body-only t ;; only export section between <body> </body>
-         :auto-sitemap t
-         :with-toc nil
-         :exclude "PrivatePage.org"
-         )
-        ("behp_manual"
-         ;; Path to your org files
-         :base-directory "~/workspace/dotfiles/.emacs.d/__org/BE"
-         :base-extension "org"
-         
-         :publishing-directory "~/workspace/temp_BEhp/document"
-         :recursive t
-         :publishing-function org-html-publish-to-html
          :html-extension "html"
-         :body-only nil ;; only export section between <body> </body>
-         :with-toc nil
-         :exclude "PrivatePage.org"
+         :body-only t ;; only export section between <body> </body>
+         ;; :auto-sitemap t
+         ;; :with-toc nil
+         ;; :exclude "PrivatePage.org"
          )
-        ("my-components" :components ("org"))
+        ("my-components" :components ("jekyll"))
         ))
