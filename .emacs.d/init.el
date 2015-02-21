7b342c8680ba7d729abbeaff4e3195ae1bee235b
Naoki Ueda <su104003@gmail.com>
2015-02-21 16:06:53 +0900

    ---

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -88,18 +88,8 @@
      (eval-print-last-sexp))))
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
-;; (unless (require 'el-get nil 'noerror)
-;;   (with-current-buffer
-;;       (url-retrieve-synchronously
-;;        "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-;;     (goto-char (point-max))
-;;     (eval-print-last-sexp)))
-
-;; (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-;; (el-get 'sync)
-
 ;; (el-get-bundle elpa:undo-tree)
-;; (el-get-bundle imakado/emacs-smartchr)
+(el-get-bundle imakado/emacs-smartchr)
 
 ;;; define el-get repository
 (setq el-get-sources
