2d9664e76683b472c419fc1e3ec5d229e2fbb8aa
Naoki Ueda <su104003@gmail.com>
2015-05-26 12:37:30 +0900

    スクラッチを保存する

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -201,6 +201,7 @@
       (package-install package)))
   )
 
+;;; ==============================================
 ;;; init-loader
 (require 'init-loader)
 (setq init-loader-show-log-after-init 'error-only)  ;エラー時のみ表示
@@ -213,3 +214,34 @@
 ;;; scaratch-pop
 (require 'scratch-pop)
 (global-set-key (kbd "C-c RET") 'scratch-pop)
+
+;;; ==============================================
+;;; save scratch buffer when exit
+(defun save-scratch-data ()
+  (let ((str (progn
+               (set-buffer (get-buffer "*scratch*"))
+               (buffer-substring-no-properties
+                (point-min) (point-max))))
+        (file "~/.emacs.d/backups/.scratch"))
+    (if (get-file-buffer (expand-file-name file))
+        (setq buf (get-file-buffer (expand-file-name file)))
+      (setq buf (find-file-noselect file)))
+    (set-buffer buf)
+    (erase-buffer)
+    (insert str)
+    (save-buffer)
+    (kill-buffer buf)))
+
+(defadvice save-buffers-kill-emacs
+  (before save-scratch-buffer activate)
+  (save-scratch-data))
+
+(defun read-scratch-data ()
+  (let ((file "~/.emacs.d/backups/.scratch"))
+    (when (file-exists-p file)
+      (set-buffer (get-buffer "*scratch*"))
+      (erase-buffer)
+      (insert-file-contents file))
+    ))
+
+(read-scratch-data)
